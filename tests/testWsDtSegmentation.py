679f06e583eb29596b545a7061e3980c7a0b2480
Stuart Berg <bergs@janelia.hhmi.org>
2016-03-22 18:40:48 -0400

    tests: Added test for memory usage. (Current code version needs 6.3x extra RAM...)

--- tests/testWsDtSegmentation.py
+++ tests/testWsDtSegmentation.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 
 from wsdt import wsDtSegmentation
+from numpy_allocation_tracking.decorators import assert_mem_usage_factor
 
 class TestWsDtSegmentation(unittest.TestCase):
 
@@ -86,6 +87,52 @@ class TestWsDtSegmentation(unittest.TestCase):
         assert seeds.sum() == 1
         assert seeds[0, 25, 25] == 1
 
+    def test_memory_usage(self):
+        # Wrap the segmentation function in this decorator, to verify it's memory usage.
+        memchecked_wsDtSegmentation = assert_mem_usage_factor(7.0)(wsDtSegmentation)
+
+        # Create a volume with 8 sections
+        pmap = np.zeros( (101,101,101), dtype=np.float32 )
+
+        # Three Z-planes
+        pmap[:2, :, :] = 1
+        pmap[49:51, :, :] = 1
+        pmap[-2:, :, :] = 1
+
+        # Three Y-planes
+        pmap[:, :2, :] = 1
+        pmap[:, 49:51, :] = 1
+        pmap[:, -2:, :] = 1
+
+        # Three X-planes
+        pmap[:, :, :2] = 1
+        pmap[:, :, 49:51] = 1
+        pmap[:, :, -2:] = 1
+
+
+        # First, just check the seeds
+        seeds = memchecked_wsDtSegmentation(pmap, 0.5, 0, 10, 0.1, 0.1, cleanCloseSeeds=False, returnSeedsOnly=True)
+        assert seeds.max() == 8
+
+        # Expect seeds at (25,25,25), (25,25,75), (25,75,25), etc...
+        expected_seed_coords = list(np.ndindex((2,2,2)))
+        expected_seed_coords = 50*np.array(expected_seed_coords) + 25
+
+        #print "EXPECTED:\n", expected_seed_coords
+        #print "SEEDS:\n", np.array(np.where(seeds)).transpose()
+
+        for seed_coord in expected_seed_coords:
+            assert seeds[tuple(seed_coord)], "No seed at: {}".format( seed_coord )
+
+        # Now check the whole watershed volume
+        ws_output = memchecked_wsDtSegmentation(pmap, 0.5, 0, 10, 0.1, 0.1, cleanCloseSeeds=False)
+        assert ws_output.max() == 8
+
 
 if __name__ == "__main__":
+    import sys
+    import logging
+    mem_logger = logging.getLogger('numpy_allocation_tracking')
+    mem_logger.setLevel(logging.DEBUG)
+    mem_logger.addHandler(logging.StreamHandler(sys.stdout))
     unittest.main()
