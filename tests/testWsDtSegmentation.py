a19935ea70c01a576424a678ce65ae5e76061b69
Stuart Berg <bergs@janelia.hhmi.org>
2016-03-22 17:31:23 -0400

    tests: Added a simple test for 2D

--- tests/testWsDtSegmentation.py
+++ tests/testWsDtSegmentation.py
@@ -5,7 +5,7 @@ from wsdt import wsDtSegmentation
 
 class TestWsDtSegmentation(unittest.TestCase):
 
-    def test1(self):
+    def test_simple_3D(self):
         # Create a volume with 8 sections
         pmap = np.zeros( (101,101,101), dtype=np.float32 )
 
@@ -42,8 +42,38 @@ class TestWsDtSegmentation(unittest.TestCase):
         ws_output = wsDtSegmentation(pmap, 0.5, 0, 10, 0.1, 0.1, cleanCloseSeeds=False)
         assert ws_output.max() == 8
 
+    def test_simple_2D(self):
+        # Create an with 4 regions
+        pmap = np.zeros( (101,101), dtype=np.float32 )
 
-    def test2(self):
+        pmap[:2, :] = 1
+        pmap[49:51, :] = 1
+        pmap[-2:, :] = 1
+
+        pmap[:, :2] = 1
+        pmap[:, 49:51] = 1
+        pmap[:, -2:] = 1
+
+        # First, just check the seeds
+        seeds = wsDtSegmentation(pmap, 0.5, 0, 10, 0.1, 0.1, cleanCloseSeeds=False, returnSeedsOnly=True)
+        assert seeds.max() == 4
+
+        # Expect seeds at (25,25,25), (25,25,75), (25,75,25), etc...
+        expected_seed_coords = list(np.ndindex((2,2)))
+        expected_seed_coords = 50*np.array(expected_seed_coords) + 25
+
+        #print "EXPECTED:\n", expected_seed_coords
+        #print "SEEDS:\n", np.array(np.where(seeds)).transpose()
+
+        for seed_coord in expected_seed_coords:
+            assert seeds[tuple(seed_coord)], "No seed at: {}".format( seed_coord )
+
+        # Now check the whole watershed volume
+        ws_output = wsDtSegmentation(pmap, 0.5, 0, 10, 0.1, 0.1, cleanCloseSeeds=False)
+        assert ws_output.max() == 4
+
+
+    def test_border_seeds(self):
         # check if seeds at the borders are generated
 
         # create a volume with membrane evidence everywhere
