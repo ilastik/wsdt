dddbae3d17b062d37568ea8b155b1cd33cb4a850
k-dominik <k-dominik@users.noreply.github.com>
2019-02-18 16:10:25 +0100

    cleanup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='wsdt',
-      version='0.1',
+      version='0.2',
       description='Implementation of a distance-transform-based watershed algorithm',
       author='Timo Prange',
       author_email='timo.prange@iwr.uni-heidelberg.de',
